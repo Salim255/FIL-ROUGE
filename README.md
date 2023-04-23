9de415e3a349da1669af11bedfb3c8be80933271
Salim <salim.hassan.welleat@gmail.com>
2023-04-23 13:24:48 +0200

    Running: frontend and backend docker images

--- README.md
+++ README.md
@@ -8,6 +8,18 @@
 
 ## npm install axios
 
+# 🍎 Docker Section:
+
+# 🍌 back end part
+
 ## docker build -t backend .
 
 ## docker run -d --rm -p 3000:3000 backend
+
+## npm i cors
+
+# 🍌 Front end part
+
+## docker build -t frontend .
+
+## docker run --rm -d -p 3001:3000 frontend
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+
 const app = express();
+
 app.get("/test", function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+
   res.status(200).json({
     status: "success",
     message: "Hello world, from Salim Fil Rouge",
