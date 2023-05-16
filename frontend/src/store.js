eb3ef1d15eb3eac15eb4a1acbb9994fe2fec7832
Salim <salim.hassan.welleat@gmail.com>
2023-05-16 13:25:31 +0200

    new frontend

--- frontend/src/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import salim from "./feature/test/testSlice";
-export const store = configureStore({
-  reducer: {
-    test: salim,
-  },
-});
--- frontend/src/utils/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-const customFetch = axios.create({
-  baseURL: "http://localhost:3000",
-});
-
-export default customFetch;
