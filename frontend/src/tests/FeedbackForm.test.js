3f3cd9dda497ef7783d85437bc971b01197f284d
Salim <salim.hassan.welleat@gmail.com>
2023-05-18 11:40:57 +0200

    Add: Describe around tests

--- frontend/src/tests/FeedbackForm.test.js
+++ frontend/src/tests/FeedbackForm.test.js
@@ -1,21 +1,34 @@
 import { screen, render } from "@testing-library/react";
 import FeedbackForm from "../components/FeedbackForm";
+describe("Test related to feedback Form", () => {
+  test("On load condtion of form", () => {
+    render(<FeedbackForm />);
 
-test("On load condtion of form", () => {
-  render(<FeedbackForm />);
+    const textbox = screen.getByRole("textbox");
+    expect(textbox).toBeInTheDocument();
 
-  const textbox = screen.getByRole("textbox");
-  expect(textbox).toBeInTheDocument();
+    const checkBox = screen.getByLabelText("I accept the terms and Condtions", {
+      exact: false,
+    });
+    expect(checkBox).toBeInTheDocument();
 
-  const checkBox = screen.getByLabelText("I accept the terms and Condtions", {
-    exact: false,
+    const btn = screen.getByRole("button", {
+      name: "Add Feedback",
+      exact: false,
+    });
+    expect(btn).toBeInTheDocument();
+    expect(btn).toBeDisabled();
   });
-  expect(checkBox).toBeInTheDocument();
 
-  const btn = screen.getByRole("button", {
-    name: "Add Feedback",
-    exact: false,
+  test("Button enabled only if input and checkbox have values", () => {
+    render(<FeedbackForm />);
+    const textbox = screen.getByPlaceholderText("Enter your Feedback here");
+    const checkBox = screen.getByLabelText("I accept the terms and Condtions", {
+      exact: false,
+    });
+    const btn = screen.getByRole("button", {
+      name: "Add Feedback",
+      exact: false,
+    });
   });
-  expect(btn).toBeInTheDocument();
-  expect(btn).toBeDisabled();
 });
